3d10d6d42cc1510d3a4565f6decf78ded794e2b7
Francisco Kluser <franciscogkoclavero@gmail.com>
2023-01-17 10:23:38 -0300

    feat: insert redux in tests

--- src/components/HourCell/index.test.tsx
+++ src/components/HourCell/index.test.tsx
@@ -1,9 +1,15 @@
 import { render, screen } from "@testing-library/react";
+import { Provider } from "react-redux";
+import { store } from "../../redux/store";
 import HourCell from ".";
 
 describe("Hour Cell", () => {
   beforeEach(() => {
-    render(<HourCell hour="00:30"/>)
+    render(
+      <Provider store={store}>
+        <HourCell dayWeek="dia" hour="00:30"/>
+      </Provider>
+    )
   });
   
   it("Hour Cell render", () => {
